c848cf8ff0ae23da8bedf2461cebecd7da698d28
ahmadsgamer2 <107987829+ahmadsgamer2@users.noreply.github.com>
2024-03-23 00:51:43 +0400

    Update Setting.lua

--- ZekromLib/V2/Setting.lua
+++ ZekromLib/V2/Setting.lua
@@ -1,1 +1,11 @@
 _G.Discord = "https://discord.gg/tusaQzURPQ"
+
+if getgenv().AntiLag then
+	local renderStepped = game:GetService("RunService").RenderStepped
+	local function GetFPS() 
+		renderStepped:Wait() 
+		local V = 0 for _ = 1, 5 do V += 1 / renderStepped:Wait() 
+		end return math.round(V / 5) 
+	end
+     spawn(function()setfpscap(GetFPS())end)
+end
